586b34b3cae7086cae9eb0f82f447a2f03943938
brahim024 <ifninos168@gmail.com>
2020-11-11 18:01:54 +0100

    comment

--- ch01/ex05.py
+++ ch01/ex05.py
@@ -1,5 +1,12 @@
 '''Exercise 5: Write a program which prompts the user for a Celsius temperature,
 convert the temperature to Fahrenheit, and print out the converted temperature.'''
+
+# How to convert Celsius to Fahrenheit
+# The temperature T in degrees Fahrenheit (°F) is equal to the temperature T in degrees Celsius (°C) times 9/5 plus 32:
+# T(°F) = T(°C) × 9/5 + 32
+# or
+# T(°F) = T(°C) × 1.8 + 32
+
 cels=float(input('enter you  Celsius temperature: '))
-fahrenheit=cels*9/5+32
-print(fahrenheit)
+fahrenheit=cels*9/5+32 
+print("fahrenheit: ",fahrenheit)
