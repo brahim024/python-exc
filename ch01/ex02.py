83fccd38425c50f03f67cb791c81e7cc39a1bb21
brahim024 <ifninos168@gmail.com>
2020-11-11 16:37:58 +0100

    solution of two exc

---
+++ ch01/ex02.py
@@ -0,0 +1,6 @@
+'''Exercise 2: Write a program that uses input to prompt a user for their name 
+and then welcomes them.'''
+name=input('enter your name: ')
+print(f'{name} you are welcome')
+
+
---
+++ ch01/ex03.py
@@ -0,0 +1,7 @@
+'''Exercise 3: Write a program to prompt the user for hours and rate per hour to
+compute gross pay.'''
+
+hour=int(input("enter your hours!: "))
+rate=float(input("enter your rate!: "))
+pay=hour*rate
+print('your pay is: ',pay)
