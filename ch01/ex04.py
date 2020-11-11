88dc62229c7c65eb58d04ccd68ed938ba686c671
brahim024 <ifninos168@gmail.com>
2020-11-11 17:28:30 +0100

    add 04 and 05 exc

---
+++ ch01/ex04.py
@@ -0,0 +1,7 @@
+
+width=17
+height=12.0
+
+print(type(width//2))
+print(type(width/2.0))
+print(type(height/3))
---
+++ ch01/ex05.py
@@ -0,0 +1,5 @@
+'''Exercise 5: Write a program which prompts the user for a Celsius temperature,
+convert the temperature to Fahrenheit, and print out the converted temperature.'''
+cels=float(input('enter you  Celsius temperature: '))
+fahrenheit=cels*9/5+32
+print(fahrenheit)
