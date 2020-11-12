b9a45b10f6cde6ec8b3b0151ca43cce69b90769f
brahim024 <ifninos168@gmail.com>
2020-11-12 12:58:11 +0100

    add try exception

--- ch03/ex03.py
+++ ch03/ex03.py
@@ -1,18 +1,21 @@
 # Exercise 3: Write a program to prompt for a score between 0.0 and 1.0. If the
 # score is out of range, print an error message. If the score is between 0.0 and 1.0,
 # print a grade using the following table:
-
-
 while True:
-	score=float(input('enter your score: '))
-	if score >= 0.9:
-		print('A')
-	elif score >= 0.8:
-		print('B')
-	elif score >= 0.7:
-		print('C')
-	elif score >= 0.6:
-		print('D')
-	else:
-		print('F')
+	score=input('enter your score: ')
+	try:
+		score=float(score)
+		if score >= 0.9:
+			print('A')
+		elif score >= 0.8:
+			print('B')
+		elif score >= 0.7:
+			print('C')
+		elif score >= 0.6:
+			print('D')
+		else:
+			print('F')
+	except ValueError:
+		print('Bad score')
+
 
