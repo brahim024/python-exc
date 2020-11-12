d0250b54bc8dbe7403f28b66b0d602255ae16ddb
brahim024 <ifninos168@gmail.com>
2020-11-12 12:09:32 +0100

    add exc comment

--- ch01/ex03.py
+++ ch01/ex03.py
@@ -1,7 +1,11 @@
 '''Exercise 3: Write a program to prompt the user for hours and rate per hour to
 compute gross pay.'''
-
+# Rewrite your pay computation to give the employee 1.5 times the
+# hourly rate for hours worked above 40 hours
 hour=int(input("enter your hours!: "))
 rate=float(input("enter your rate!: "))
-pay=hour*rate
+if hour > 40:
+	pay=(hour - 40)*rate*2 +40 *rate
+else:
+	pay=hour*rate
 print('your pay is: ',pay)
